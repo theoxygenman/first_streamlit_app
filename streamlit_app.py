7ccdeb81dad6e3e98d59e36f014d672eb218e943
J <112618017+theoxygenman@users.noreply.github.com>
2022-09-01 10:14:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 kale, Spinach, Rocket Smoothie")
 streamlit.text("🐔 🐔 Hard Boiled Free Range VEGAN Egg")
 streamlit.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
