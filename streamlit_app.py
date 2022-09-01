0961f868fefbdf8df29987b9784b06ccce11f7dd
J <112618017+theoxygenman@users.noreply.github.com>
2022-09-01 10:18:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title("Hello welcome to JenJen's new healthy diner, wow")
 streamlit.header("Breakfast Menu")
 
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗 kale, Spinach, Rocket Smoothie")
+streamlit.text("🥗 kale, Spinach, Apple & Rocket Smoothie")
 streamlit.text("🐔 🐔 Hard Boiled Free Range VEGAN Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
