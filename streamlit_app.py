b10efdab0d3def2d392b759742dc88edbfd92cd9
J <112618017+theoxygenman@users.noreply.github.com>
2022-09-01 10:12:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 
-streamlit.title("My parents new healthy diner, wow")
+streamlit.title("Hello welcome to JenJen's new healthy diner, wow")
 
 streamlit.header("Breakfast Menu")
 
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 kale, Spinach, Rocket Smoothie")
-streamlit.text("🐔 🐔Hard boiled free range egg")
+streamlit.text("🐔 🐔Hard boiled free range VEGAN egg")
 streamlit.text("🥑🍞 Avocada Toast")
